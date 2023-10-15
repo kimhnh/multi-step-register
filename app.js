8b521fd9ff64a3f5a075ae0e58b4c903b9353f8a
slddays <kimhnh@gmail.com>
2023-10-15 13:58:42 +0900

    second push before rewriting logic

---
+++ app.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('.btn');
+const heading = document.querySelector('.heading');
+const card = document.querySelector('.card');
+
+// btn.addEventListener('click', function (e) {
+//   console.log('button clicked');
+//   heading.innerText = 'heading changed';
+//   card.innerHTML = `<p>hello</p>`;
+// });
+
+// write all 3 divs in html, include ids
+// write logic to toggle?, somehow flip through them
+// figure out why once i click continue the form ends
