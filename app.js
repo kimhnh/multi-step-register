bdf9192a4bc4b2de28819f71023980bb423c633e
slddays <kimhnh@gmail.com>
2023-10-17 00:54:07 +0900

    changed inline style to classList add/remove. logic to add lis based on interests

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cardOne = document.getElementById('cardOne');
 const cardTwo = document.getElementById('cardTwo');
 const cardThree = document.getElementById('cardThree');
 const cardFinal = document.getElementById('cardFinal');
+const flashMessage = document.querySelector('.flash-message');
 
 // elements
 const heading = document.querySelector('.header');
@@ -15,43 +16,63 @@ const name = document.getElementById('name');
 const email = document.getElementById('email');
 const nameSubmit = document.getElementById('nameSubmit');
 const emailSubmit = document.getElementById('emailSubmit');
-const softwareDev = document.getElementById('software-dev');
-const ux = document.getElementById('ux');
-const graphic = document.getElementById('graphic');
 
 ///////////////////////////////////////////////////////////////
 
 // todo list:
-// 1. add flash message for error (div in html)
-// 2. logic for step label (progress)
-// 3. disable url to show query from our form submission
+// 1. add logic to step 2: if any checkbox is checked ... else flash error message
+// 2. logic for step label (progress indicator)
+// 3. disable query to show in url
 // 4. refine CSS (this should be last)
+// 5. email input (right now you can bypass it without even having @) -- come back to this later
 
 ///////////////////////////////////////////////////////////////
 
-// Step 1: Name&Email (flash message logic)
+// Step 1: Name&Email
 btnStepOne.addEventListener('click', function (e) {
   e.preventDefault();
   if (name.value && email.value) {
-    cardOne.style.display = 'none';
-    cardTwo.style.display = 'flex';
+    cardOne.classList.add('hidden');
+    cardTwo.classList.remove('hidden');
     nameSubmit.textContent = name.value; // must have code here to dynamically change in step 3
     emailSubmit.textContent = email.value;
-  } else {
-    console.log('Something went wrong. Please try again.');
   }
 });
 
-// Step 2: Interests (requires css update)
+// Step 2: Interests - require css update (flash message if nothing is checked)
 btnStepTwo.addEventListener('click', function (e) {
   e.preventDefault();
-  cardTwo.style.display = 'none';
-  cardThree.style.display = 'flex';
+  cardTwo.classList.add('hidden');
+  cardThree.classList.remove('hidden');
+  let checkedValues = document.querySelectorAll('[type="checkbox"]:checked');
+  let printValues = Array.from(checkedValues, (el) => el.value);
+
+  // dynamically create lis by looping through an array
+  for (let i = 0; i < printValues.length; i++) {
+    let list = document.createElement('li');
+    list.textContent = printValues[i];
+    document.getElementById('interestList').appendChild(list);
+  }
 });
 
 // Step 3: Summary
 btnStepThree.addEventListener('click', function (e) {
   e.preventDefault();
-  cardThree.style.display = 'none';
-  cardFour.style.display = 'flex';
+  cardThree.classList.add('hidden');
+  cardFinal.classList.remove('hidden');
 });
+
+// Logic to add checked values into an array (for loop)
+//inside event listener
+// displayChecked();
+// console.log(checkedValues);
+
+// let checkedValues = [];
+
+// function displayChecked() {
+//   checkedValues = [];
+//   let checked = document.querySelectorAll('[type="checkbox"]:checked');
+//   for (let i = 0; i < checked.length; i++) {
+//     checkedValues.push(checked[i].value);
+//   }
+// }
